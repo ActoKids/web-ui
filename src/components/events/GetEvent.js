11ed791dd237977a53cf5ced44062d0c16a169eb
Hau Khual <haukhual08@gmail.com>
2019-03-01 20:55:57 -0800

    updated getEvent.js

--- src/components/events/GetEvent.js
+++ src/components/events/GetEvent.js
@@ -10,13 +10,13 @@ export default class getEvents extends Component{
 
 componentDidMount() {
     // Where we're fetching data from
-    fetch(`https://jsonplaceholder.typicode.com/users`)
+    fetch(`https://api-zakb.2edusite.com/v1/v1/events`)
       // We get the API response and receive data in JSON format...
       .then(response => response.json())
       // ...then we update the users state
       .then(data =>
         this.setState({
-          users: data,
+          users: data.Items,
           isLoading: false,
         })
       )
@@ -31,17 +31,51 @@ render() {
         <div className="container">
             <React.Fragment>
                 <h3>Fetching Data from remote API</h3>
-                <h5>Random User</h5>
+               
             {error ? <p>{error.message}</p> : null}
             {!isLoading ? (
                 users.map(user => {
-                const { username, name, email } = user;
+                const{id, contact_name, description, contact_email, location_address } = user;
                 return (
-                    <div key={username}>
-                    <p>Name: {name}</p>
-                    <p>Email Address: {email}</p>
-                    <hr />
-                    </div>
+
+
+                    // <div key={id}>
+                    /* <p>Name: {contact_name}</p>
+                    <p>Description: {description}</p>
+                    <p>Email Address: {contact_email}</p>
+                    <hr /> */
+                 
+          <div key={id}>
+
+              <div class="row">
+              <div class="col s12 m6">
+                <div class="card blue-grey darken-1">
+                  <div class="card-content white-text">
+                    <span class="card-title">{contact_name}</span>
+                    <p><b>Description:</b> {description}</p>
+                    <p><b>Location:</b> {location_address}</p>
+                    <p><b>Contact Email:</b> {contact_email}</p>
+                  </div>
+                  {/* <div class="card-action">
+                    <a href="#">This is a link</a>
+                    <a href="#">This is a link</a>
+                  </div> */}
+                </div>
+              </div>
+              </div>
+
+          </div>
+                
+
+
+
+
+
+
+
+
+
+
                 );
                 })
             // If there is a delay in data, let's let the user know it's loading

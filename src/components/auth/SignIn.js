42944581a3b59f2a121eab248538dd80a1d73294
coultergeist <dcoult01@seattlecentral.edu>
2019-03-06 15:06:58 -0800

    fixed signin

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,24 +1,48 @@
 import React, { Component } from 'react'
+import { Auth } from 'aws-amplify';
 
-class SignIn extends Component {
-    state = {
-        email: '',
-        password: ''
-    }
 
-    handleChange = (e) => {
-        this.setState({
-            // targets the id and its value
-            [e.target.id]: e.target.value
-        })
-    }
+export default class SignIn extends React.Component {
     
-    handleSubmit = (e) => {
-        e.preventDefault();
-        // shows whatever the user has typed into the form
-        console.log(this.state);
-    }
+    constructor(props) {
+		super(props);
+        //set properties of the state
+		this.state = {
+			isLoading: false,
+			email: 'test@zach.com',
+			password: 'password2'
+        };
+    
+	}
+    //determining the change of the selected target/state
+	handleChange = event => {
+		this.setState({
+			[event.target.id]: event.target.value
+		});
+	};
 
+    //either when you select Login or Enter, perform this action
+	handleSubmit = async event => {
+        //promise is made that async request will not effect load time
+        this.setState({ isLoading: true });
+
+        //pull credentials from state and checking if authentication is true
+		try {
+            await Auth.signIn(this.state.email, this.state.password)
+            .then( () => 
+            console.log("Inside handleSubmit, before this.props"),
+            console.log("props: " + this.props.route),
+			this.props.userHasAuthenticated(true),
+            //now load dashboard to DOM
+            this.props.history.push('/dashboard')
+            
+            //if auth fails or not possible
+        )} catch (e) {
+			alert(e.message);
+			this.setState({ isLoading: false });
+		}
+	};
+    
     render() {
         return (
             <div className="container">
@@ -26,11 +50,11 @@ class SignIn extends Component {
                     <h5 className="grey-text text-darken-3">Sign In</h5>
                     <div className="input-field">
                         <label htmlFor="email">Email</label>
-                        <input type="email" id="email" onChange={this.handleChange} required/>
+                        <input type="email" id="email" value={this.state.email} onChange={this.handleChange} required/>
                     </div>
                     <div className="input-field">
                         <label htmlFor="password">Password</label>
-                        <input type="password" id="password" onChange={this.handleChange} required/>
+                        <input type="password" id="password" value={this.state.password} onChange={this.handleChange} required/>
                     </div>
                     <div className="input-field">
                         <button className="btn pink lighten-1">Log in</button>
@@ -40,5 +64,3 @@ class SignIn extends Component {
         )
     }
 }
-
-export default SignIn

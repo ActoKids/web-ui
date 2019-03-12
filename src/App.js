da013f7379adc076bdf8e27742e18894ab606d9c
Erik Neville <erikeneville@gmail.com>
2019-03-11 21:39:47 -0700

    changed the AppliedRoute for the dashboard page to 'events' page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { BrowserRouter, Switch, Route, PropsRoute  } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import EventDetails from './components/events/EventDetails';
+import getDetails from './components/events/getDetails'
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
 import UserForm from "./components/events/UserForm";
 import UpdateEvent from './components/events/UpdateEvent';
 import GetEvent from './components/events/GetEvent';
+import events from './components/events/events'
 
 
 import AppliedRoute from './components/AppliedRoute';
@@ -64,7 +66,7 @@ class App extends Component {
           <Switch>
             <AppliedRoute exact path='/' component={ SignIn } props={childProps} />
             <AppliedRoute path='/event/:id' component={ EventDetails } props={childProps}/>
-            <AppliedRoute path='/dashboard' component={ Dashboard } props={childProps}/>
+            <AppliedRoute path='/events' component={ events } props={childProps}/>
             <AppliedRoute path='/signup' component={ SignUp } props={childProps}/>
             <AppliedRoute path='/create' component={ UserForm } props={childProps}/>
             <AppliedRoute path='/update' component={ UpdateEvent } props={childProps}/>

e02e5e814ad3ed9e6bee39f9e5d672507ec6cdcc
Erik Neville <erikeneville@gmail.com>
2019-03-17 16:33:41 -0700

    attempting to format the date-time properly

--- src/components/events/EventSummary.js
+++ src/components/events/EventSummary.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import moment from 'moment'
 
 // The information that is displayed on each card in the
 // EventList on the Dashboard. Props come from EventList which
@@ -13,6 +14,7 @@ const EventSummary = ({event}) => {
                 <div className="card-content grey-text text-darken-3">
                     <span className="card-title">{event.event_name} at: {event.location_name}</span>
                     <p>Posted By {event.contact_name}</p>
+                    <p className="grey-tex">{event.start_date_time}</p>
                     <p className="grey-text">Ages: {event.min_age} - {event.max_age}</p>
                 </div>
             </div>

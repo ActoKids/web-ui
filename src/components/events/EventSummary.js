ccb2795b58fddd3e3f9b826d3fc4c3e7cc3b9453
Erik Neville <erikeneville@gmail.com>
2019-03-18 15:33:00 -0700

    Moved the image to align with the event title and made it a little smaller

--- src/components/events/EventSummary.js
+++ src/components/events/EventSummary.js
@@ -12,10 +12,7 @@ const EventSummary = ({event}) => {
         <Link to={{pathname: '/events/' + event.event_id, state: {event: event}}}>
             <div className="card horizontal hoverable events-summary">
                 <div className="card-content grey-text text-darken-3">
-      
-                   <img src={event.picture_url}  width="50" height="50"/>
-
-                    <span className="card-title">{event.event_name} at: {event.location_address}</span>
+                    <span className="card-title valign-wrapper"><img src={event.picture_url} width="30" height="30" /> {event.event_name} at: {event.location_address}</span>
                     <p>Posted By {event.user_name}</p>
                     <Moment format="h:mm a - MM/DD/YYYY">{event.start_date_time}</Moment>
                     <p className="grey-text">Something Else Here</p>

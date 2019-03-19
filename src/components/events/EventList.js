993f59e785c69ace4ca6099ac518a768ca4d73d8
Yahya Hussein <yahyahussein91@gmail.com>
2019-03-19 13:38:27 -0700

    added delete functionality to eventlist

--- src/components/events/EventList.js
+++ src/components/events/EventList.js
@@ -14,7 +14,86 @@ import EventSummary from './EventSummary'
 // props comes from Dashboard.js but we are only passing
 // an array of {events} objects into this UI component
 
+// Filled with EventSummary.js items
+class EventList extends Component {
+  static propTypes = {
+    deleteEvents: PropTypes.func.isRequired,
+    Options: PropTypes.func,
+    history: PropTypes.shape({
+      push: PropTypes.func.isRequired
+    }).isRequired,
+    events: PropTypes.array.isRequired
+  };
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedItemIds: []
+    };
+  }
+
+  /**
+   * Go to event page on click
+   */
+  handleOnClickEvent = itemId => () => {
+    this.props.history.push(`/event/${itemId}`);
+  };
+
+  /**
+   * Handle select to check or uncheck event
+   */
+  handleOnSelectEvent = itemId => event => {
+    const targetCheck = event.target.checked;
+
+    this.setState({
+      selectedItemIds: targetCheck
+        ? [...this.state.selectedItemIds, itemId]
+        : this.state.selectedItemIds.filter(id => id !== itemId)
+    });
+  };
+
+  /**
+   * Delete events
+   */
+  handleDeleteItems = () => {
+    const { selectedItemIds } = this.state;
+    if (selectedItemIds && selectedItemIds.length > 0) {
+      // dispatch an action to remove selected items
+      if (this.props.deleteEvents) {
+        this.props.deleteEvents(selectedItemIds);
+      }
+
+      const newStateOfSelectedIds = [...selectedItemIds];
+
+      // Iterate over selected IDs and delete them from API
+      selectedItemIds.forEach((id, i) => {
+        API.del("events", `/events/${id}`);
+
+        newStateOfSelectedIds.splice(i, 1);
+
+        return null;
+      });
+
+      this.setState({
+        selectedItemIds: newStateOfSelectedIds
+      });
+    }
+    return null;
+  };
+
+  handleEditItems = () => {
+    return null;
+  };
+
+  handleSaveItems = () => {
+    return null;
+  };
+
+  handleGetData = () => {
+    return null;
+  };
+render() {
 const EventList = ({events}) => {
     return (
         <div className="event-list section">
@@ -43,5 +122,18 @@ const EventList = ({events}) => {
         </div>
     )
 }
+}
+
+// Maps the state from redux store to props
+const mapStateToProps = state => {
+  return {
+    // redux state, event from event property of state, events from initState
+    events: state.event.events
+  };
+};
+
+const mapDispatchToProps = {
+  deleteEvents
+};
 
 export default EventList

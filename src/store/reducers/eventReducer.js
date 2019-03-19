c98d8fd962a39412407585d5bf5ff30fcf2ba735
Yahya Hussein <yahyahussein91@gmail.com>
2019-03-19 11:29:25 -0700

    added delete case to reducer

--- src/store/reducers/eventReducer.js
+++ src/store/reducers/eventReducer.js
@@ -103,13 +103,36 @@ const initState = {
         ]
 } 
 
-const eventReducer = (state = initState, action) => {
-    // checking the type of action preformed 
-    switch (action.type) {
-        case 'CREATE_EVENT':
-            console.log('created project', action.event)
-    } 
-    return state
-}
+const eventReducer = (state = initialState, action) => {
+  // checking the type of action preformed
+  switch (action.type) {
+    case "CREATE_EVENT":
+      return state;
+    case "EVENTS_FETCHED":
+      return {
+        events: action.payload
+      };
+    // delete events
+    case "DELETE_EVENTS":
+      const eventIds = action.payload;
+      if (eventIds && eventIds.length > 0) {
+        let events = state.events;
+
+        // filter selected item ids
+        eventIds.forEach(id => {
+          events = events.filter(event => event.event_id !== id);
+          return null;
+        });
+
+        return {
+          ...state,
+          events
+        };
+      }
+      return state;
+    default:
+      return state;
+  }
+};
 
 export default eventReducer

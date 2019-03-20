8d4d5a68cef1fc776157aea6c704ff93b76485d1
Yahya Hussein <yahyahussein91@gmail.com>
2019-03-19 20:06:40 -0700

    finilazed changes for delete and error pages

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,60 +1,58 @@
-import React, { Component } from 'react'
-import { API } from 'aws-amplify';
-import Options from './Options'
-import EventList from '../events/EventList'
+import React, { Component } from "react";
+import { API } from "aws-amplify";
+import { connect } from "react-redux";
 
-import { Redirect } from 'react-router-dom'; 
+import Options from "./Options";
+import EventList from "../events/EventList";
+import { saveEvents } from "../../store/actions/eventActions";
+import { Redirect } from "react-router-dom";
 
 class Dashboard extends Component {
-    // Constructor is repsonbile for setting up props and setting initial state
-    constructor(props) {
-        //pass props to the parent component
-        super(props);
-        // State needed
-        this.state = {
-            events: [],
-            isLoading: true
-        };
-    }
+  // Calling AWS API get() method. This method takes in
+  // two parameters: the API name "events" and the API URL "/events"
+  // Then we set the state of the component to the API's response
+  getEvents() {
+    API.get("events", "/events").then(response => {
+      if (this.props.saveEvents) {
+        this.props.saveEvents(response.Items);
+      }
+    });
+  }
 
-    // Calling AWS API get() method. This method takes in
-    // two parameters: the API name "events" and the API URL "/events"
-    // Then we set the state of the component to the API's response
-    getEvents() {
-        API.get("events", "/events")
-            .then(response => {
-                this.setState({
-                    events: response.Items
-                });
-            })
-    }
+  componentDidMount() {
+    this.getEvents();
+  }
 
-    componentDidMount() {
-        this.getEvents();
-    }
+  render() {
+    const eventInfo = this.props.events;
+    const auth = this.props.isAuthenticated;
 
-    
-
-    render() {
-        const eventInfo = this.state.events;
-
-        const auth = this.props.isAuthenticated;
-
-        /*Check if the user is already authicated 
+    /*Check if the user is already authicated 
             if is not, then redirect to sign in page*/
-        if(!auth) return <Redirect to='/' />
+    if (!auth) return <Redirect to="/" />;
 
-        return (
-            // passing the state of this dashboard to EventList.js
-            // as props
-                
-            <div className="container">
-                <h3>Dashboard</h3>
-                <Options />         
-                <EventList events={eventInfo} key={eventInfo.event_id} />
-            </div>            
-        )
-    }
+    return (
+      // passing the state of this dashboard to EventList.js
+      // as props
+      <div className="container">
+        <h3>Dashboard</h3>
+        <EventList Options={Options} events={eventInfo} />
+      </div>
+    );
+  }
 }
 
-export default Dashboard
+function mapStateToProps(state) {
+  return {
+    events: state.event.events
+  };
+}
+
+const mapDispatchToProps = {
+  saveEvents
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Dashboard);
